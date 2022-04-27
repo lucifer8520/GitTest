df4a37545e65fcf3ced5a4ff316fa8924f2b604b
james <james@gmail.com>
2022-04-27 15:31:51 +0800

    first

---
+++ src/main/java/com/machu/GitTest.java
@@ -0,0 +1,16 @@
+package com.machu;
+
+/**
+ * @author: James Yan
+ * @create: 2022-04-27
+ */
+public class GitTest {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
